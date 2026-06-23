9f77823b948f51775162145b5efeda83c63af65d
Nicola Solazzo <solazzo.nicola@gmail.com>
2026-06-23 19:00:45 +0200

    ok saved admin

--- serverapp/tickets/admin.py
+++ serverapp/tickets/admin.py
@@ -1,3 +1,10 @@
+# tickets/admin.py
 from django.contrib import admin
+from .models import User, Project, ProjectMember, Ticket, Message
 
-# Register your models here.
+# Registriamo i modelli in modo semplice
+admin.site.register(User)
+admin.site.register(Project)
+admin.site.register(ProjectMember)
+admin.site.register(Ticket)
+admin.site.register(Message)
