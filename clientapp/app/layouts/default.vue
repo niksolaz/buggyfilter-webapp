87205bba36ade6632a69764b470e67b572fcae00
Nicola Solazzo <solazzo.nicola@gmail.com>
2026-09-15 12:02:29 +0200

    create Home component

--- clientapp/app/layouts/default.vue
+++ clientapp/app/layouts/default.vue
@@ -13,26 +13,27 @@ const showView = (view: string) => {
     <nav
       class="flex justify-center py-10 [&>div]:mx-2 [&>div]:text-brand-500 [&>div]:font-semibold [&>div]:hover:underline [&>div]:hover:text-brand-700 [&>div]:focus:text-brand-600"
     >
-      <div @click="showView('home')">Home</div>
-      <div @click="showView('pricing')">Pricing</div>
+      <div @click="showView('home')">
+        <div class="flex items-center justify-center gap-2 w-full">
+          <div
+            class="flex items-center justify-center size-6 border border-brand-500 rounded-full bg-brand-500"
+          >
+            <span class="text-xl text-white font-bold">B</span>
+          </div>
+          <div class="font-bold text-xl text-brand-500 tracking-tight">
+            BuggyFilter
+          </div>
+        </div>
+      </div>
+      <div
+        class="font-bold text-xl text-brand-500 tracking-tight"
+        @click="showView('pricing')"
+      >
+        Prezzo
+      </div>
     </nav>
     <main class="flex min-h-screen items-start justify-center p-4">
-      <div class="w-full max-w-md">
-        <div class="mb-6 text-center">
-          <div class="flex items-center justify-center gap-2 w-full py-6">
-            <div
-              class="flex items-center justify-center size-12 border border-brand-500 rounded-full bg-brand-500"
-            >
-              <span class="text-4xl text-white font-bold">B</span>
-            </div>
-            <span class="font-bold text-4xl text-brand-500 tracking-tight"
-              >BuggyFilter</span
-            >
-          </div>
-          <p class="mt-1 text-sm text-[var(--color-text-muted)]">
-            Segnalazione e triage intelligente dei bug
-          </p>
-        </div>
+      <div class="w-full max-w-fit">
         <div>
           <slot />
         </div>
